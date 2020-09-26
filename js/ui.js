10031cac06f69d74e0f320966ffd85bd5f58dfce
RK <rakosolapov94@gmail.com>
2020-09-26 13:59:13 +0300

    ui.js deleted

--- js/ui.js
+++
@@ -1,9 +0,0 @@
-let UI = (function () {
-  const itemForm = document.getElementById("item");
-  const calories = document.getElementById("calories");
-  const addBtn = document.getElementById("add");
-  const updateBtn = document.getElementById("update");
-  const deleteBtn = document.getElementById("delete");
-
-  return {};
-})();
